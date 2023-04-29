8023047d9884bc265739db2a15c3c379d5cdb286
Yerim96 <chzh9993@naver.com>
2023-04-29 23:46:46 +0900

    review:third commit

---
+++ 실습복습/실습12복습.js
@@ -0,0 +1,48 @@
+let no1=5;
+let no2=8;
+let no3=9;
+
+
+// function add(a,b,c){
+//     return a+b+c;
+// };
+// let a=add(no1,no2,no3);
+// alert(a);
+
+// function sub(a,b){
+//     return a-b;
+// };
+// let b=sub(no1,no2);
+// alert(b);
+
+//  function divide(a,b){
+//     return a/b;
+//  };
+//  console.log(divide(no1,no2));
+ 
+
+//다른 방식으로 작성해보기
+
+
+// function add(a,b,c){
+//     return a+b+c;
+// }
+// alert(add(no1,no2,no3));
+
+// function sub(a,b){
+//     return a-b;
+// }
+// alert(sub(no1,no2));
+
+
+// function divide(a,b){
+//     return a/b
+// }
+// alert(divide(no1,no2));
+
+// function mul(a,b){
+//     return a*b
+// }
+// alert(mul(no1,no2));
+
+
